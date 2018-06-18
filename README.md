ee56dcf0f6c4ea8090ece987d43a6454fe92aba2
Andrew Henry <imandrewh@outlook.com>
2018-06-17 22:02:21 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Check My Vision for Android
+
+This is the code for the android app Check My Vision
+
+
---
+++ app/src/main/java/vision/checkmy/checkmyvision/MainActivity.java
@@ -0,0 +1,13 @@
+package vision.checkmy.checkmyvision;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
